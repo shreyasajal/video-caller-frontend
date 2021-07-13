dadda8fdecfb24d9497312721e6d5eecd2938cdd
shreyasajal <58565264+shreyasajal@users.noreply.github.com>
2021-07-13 17:09:23 +0530

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,6 @@
 import { createStore } from 'redux';
 import mainReducer from './reducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
 const store = createStore(
   mainReducer,
   composeWithDevTools()
